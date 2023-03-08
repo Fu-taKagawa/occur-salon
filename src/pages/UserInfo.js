078f7b17ee8fea3feca8119f5f0cf5a391e346e6
Fu-taKagawa <kagahaya2@gmail.com>
2023-03-08 13:35:19 +0900

    add func that enable you to change userInfo-NAME

--- src/pages/UserInfo.js
+++ src/pages/UserInfo.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React ,{useState,useContext,useEffect}from "react";
 import { Link } from "react-router-dom";
 import Header from "../component/header/header";
 import Footer from "../component/footer/footer";
+import firebase from "../config/firebase";
+import 'firebase/auth';
+import { AuthContext } from "../AuthService";
+
 const UserInfo = () =>{
+    const users = useContext(AuthContext)
+    const [name , setName] = useState('');
+    const handleChangeBtn = () =>{
+        const editBtn = document.querySelector('.userInfo__editBtn');
+        const editEndBtn = document.querySelector('.userInfo__editEndBtn');
+        editBtn.classList.add('display-none');
+        editBtn.classList.remove('display-block')
+        editEndBtn.classList.add('display-block');
+        editEndBtn.classList.remove('display-none');
+    }
+    useEffect(() => {
+        setName(users?.displayName || "");
+    }, [users]);
+    const handleRename = (e) => {
+        e.preventDefault();
+        const user = firebase.auth().currentUser;
+        if (user) { // currentUserがnullでないことを確認する
+            user.updateProfile({
+                displayName: name
+            }).then(() => {
+                console.log("ユーザー名が更新されました");
+            }).catch((error) => {
+                console.error("ユーザー名を更新できませんでした: ", error);
+            });
+        } else {
+            console.error("ユーザーがログインしていません");
+        }
+        window.location.reload(true)
+    }
     return(
         <>
             <Header/>
@@ -13,9 +46,18 @@ const UserInfo = () =>{
                         <div className="userInfo__content__box">
                             <div className="userInfo__content__box__text">
                                 <p>名前</p>
-                                <p>kagawa</p>
+                                <p>{users?.displayName}</p>
                             </div>
-                            <Link><p>編集する</p></Link>
+                            <Link className="userInfo__editBtn display-block" onClick={handleChangeBtn}>
+                                <p>編集する</p>
+                            </Link>
+                            <form onSubmit={handleRename} className="userInfo__editEndBtn display-none">
+                                <input type="text" placeholder={users?.displayName}
+                                    onChange={e=>{
+                                        setName(e.target.value)
+                                    }}/>
+                                <button type="submit">更新</button>
+                            </form>
                         </div>
                         <div className="userInfo__content__box">
                             <div className="userInfo__content__box__text">
